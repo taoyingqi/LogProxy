b065b99caca1589cf3c2dc9cf73b7aa27c9578e0
taoyingqi <taoyingqi2013@sina.com>
2015-12-22 22:12:58 +0800

    Change parameters

---
+++ com/lonel/logproxy/Client.java
@@ -0,0 +1,13 @@
+package com.lonel.logproxy;
+
+/**
+ * Created by lonel on 2015/12/22.
+ */
+public class Client {
+    public static void main(String[] args) {
+        LogHandler logHandler = new LogHandler();
+        UserManager UserManager = (UserManager) logHandler.bind(new UserManagerImpl());
+        String name = UserManager.findUser("0002");
+        System.out.println("Client.main() --- " + name);
+    }
+}
